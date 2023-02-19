718da8048dfe64e2067da57e3a8a5d4bd29e8df9
Roman Moisyeyev <roman.moisyeyev@gmail.com>
2023-02-19 10:30:47 +0200

    012 - componentization

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Header = () => {
+    return (
+        <header className='header'>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/2/2c/The_social_network.svg" alt="logo"/>
+        </header>
+    )
+}
