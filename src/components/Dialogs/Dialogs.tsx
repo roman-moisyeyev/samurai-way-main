c14973726a11ac2c9d6ab299d40c47b2e48d0ab4
Roman Moisyeyev <roman.moisyeyev@gmail.com>
2023-02-20 07:34:52 +0200

    21-Designing a Dialogs page

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,11 +1,24 @@
 import React from "react";
 import classes from './Dialogs.module.css'
 
-export const Dialogs = ()=>{
+export const Dialogs = () => {
 
-    return(
-        <div >
-            Dialogs
+    return (
+        <div className={classes.dialogs}>
+            <div className={classes.dialogsItems}>
+                <div className={classes.dialog}>Roman</div>
+                <div className={classes.dialog}>Oleg</div>
+                <div className={classes.dialog}>Oleksandr</div>
+                <div className={classes.dialog}>Sergey</div>
+                <div></div>
+            </div>
+            <div className={classes.messages}>
+                <div className={classes.message}>hi!</div>
+                <div className={classes.message}>hello!</div>
+                <div className={classes.message}>how are you?</div>
+                <div className={classes.message}>what is happened?</div>
+            </div>
+
         </div>
     )
 }
