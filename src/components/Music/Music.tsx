867a85d5a68ee3f1f339c40f9747026544f5d49e
Roman Moisyeyev <roman.moisyeyev@gmail.com>
2023-02-20 07:11:04 +0200

    19 - route, browser-router, routing 20 - NavLink

---
+++ src/components/Music/Music.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import classes from './Music.module.css'
+
+export const Music = () => {
+    return (
+       <div>
+           Wonderful music
+       </div>
+    )
+}
---
+++ src/components/News/News.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import classes from './News.module.css'
+
+export const News = () => {
+    return (
+        <div className={classes.news}>
+            <span>Instagram and Facebook users will now be able to pay for a blue tick verification, parent company Meta has announced.
+
+            Meta Verified will cost $11.99 (£9.96) a month on web, or $14.99 for iPhone users.</span>
+
+            It will be available in Australia and New Zealand this week.
+
+            Mark Zuckerberg, Meta chief executive, said the move will improve security and authenticity on the social media apps.
+
+            The move comes after Elon Musk, owner of Twitter, implemented the premium Twitter Blue subscription in November 2022.
+
+            Meta's paid subscription service is not yet available for businesses, but any individual can pay for verification.
+
+            Badges - or "blue ticks"- have been used as verification tools for high-profile accounts to signify their authenticity.
+
+            The subscription would give paying users a blue badge, increased visibility of their posts, protection from impersonators and easier access to customer service, Meta said in a post on their website.
+
+            The company told the BBC the change would not affect previously verified accounts, but noted there would be an increase in visibility for some smaller users who become verified thanks to the paid feature.
+
+            Allowing paying users access to a blue tick has previously caused trouble for other social media platforms.
+
+            Twitter's pay-for verification feature was paused last November when people started impersonating big brands and celebrities by paying for the badge.
+
+            Meta said Instagram and Facebook usernames will have to match a government supplied ID document to be granted verification, and users will have to have a profile picture that includes their face.
+
+            Other websites like Reddit, YouTube and Discord similarly use subscription-based models.
+
+            Meta has not yet specified when the feature will be rolled out to other countries, although Mr Zuckerberg said in a post it would be "soon".
+
+            In November, the company announced 11,000 job losses as a result of over-investment during the Covid-19 pandemic.
+
+            At the time, Mr Zuckerberg said he had predicted an increase in Meta's growth based on the rise it had over the pandemic, but that ultimately did not happen.
+
+            "Many people predicted this would be a permanent acceleration," he wrote, "I did too, so I made the decision to significantly increase our investments."
+
+            Instead he said "macroeconomic downturn" and "increased competition" caused revenue to be much lower than expected.
+
+            "I got this wrong, and I take responsibility for that," he said at the time.</div>
+    )
+}
