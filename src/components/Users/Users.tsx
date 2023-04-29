3eeab2b41267150654f0b3a0e24177d0575af724
Roman Moisyeyev <roman.moisyeyev@gmail.com>
2023-04-29 09:36:46 +0300

    54-React JS practice-life cycle, life cycle methods, componentDidMount.

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,35 +1,40 @@
 import React from "react";
-import  styles from './users.module.css'
+import styles from './users.module.css'
 import userPhoto from '../../assets/images/users.png'
-import { UserType } from "../../redux/users-reducer";
+import {UserType} from "../../redux/users-reducer";
 import axios from "axios";
+import {UsersPropsType} from "./UsersContainer";
 
 class Users extends React.Component {
 
-    constructor (props:any){
-super(props);
-
-        axios.get('https://social-network.samuraijs.com/api/1.0/users').then((response: { data: { items: UserType[]; }; })=>{
-            // @ts-ignore
-            this.props.setUsers(response.data.items)})
+    constructor(props: UsersPropsType) {//конструктор можно не писать
+        super(props);
     }
 
+    componentDidMount() {
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then((response: { data: { items: UserType[]; }; }) => {
+            // @ts-ignore
+            this.props.setUsers(response.data.items)
+        })
+    }
 
-    render(){
+    render() {
         // @ts-ignore
 
         return (
             <div>
                 {this.props.usersPage.users.map(u => <div key={u.id}>
                 <span>
-                    <div>< img src={u.photos.small !=null ? u.photos.small :userPhoto} className={styles.userPhoto}
+                    <div>< img src={u.photos.small != null ? u.photos.small : userPhoto} className={styles.userPhoto}
                                alt="user photo"/></div>
                     <div>
-                        {u.followed?
-                            <button onClick={()=>{ // @ts-ignore
-                                this.props.follow(u.id)}}>FOLLOW</button>
-                            :<button onClick={()=>{ // @ts-ignore
-                                this.props.unFollow(u.id)}}>UNFOLLOW</button>}
+                        {u.followed ?
+                            <button onClick={() => { // @ts-ignore
+                                this.props.follow(u.id)
+                            }}>FOLLOW</button>
+                            : <button onClick={() => { // @ts-ignore
+                                this.props.unFollow(u.id)
+                            }}>UNFOLLOW</button>}
 
                     </div>
                 </span>
